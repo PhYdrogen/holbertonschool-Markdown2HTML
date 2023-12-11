bb679af1dfb91e18da9a59aff7c88fefefe96265
PhYdrogen <Fossecruor@gmail.com>
2023-12-11 14:12:05 +0000

    feat: add list in parser

--- markdown2html.py
+++ markdown2html.py
@@ -9,12 +9,23 @@ def main():
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
     try:
+        ul = False
         file = open(sys.argv[1], "r")
+        file_len = open(sys.argv[1], "r")
         html = open(sys.argv[2], "w")
-        for line in file:
+        l = len(file_len.readlines())
+        for (fidx, line) in enumerate(file):
             for (idx, char) in enumerate(line):
                 if char == " " and line[idx-1] == "#":
                     print(f"<h{idx}>{line[idx:-1]}</h{idx}>", file=html)
+                    ul = False
+                if char == " " and line[idx-1] == "-":
+                    if ul is False:
+                        print(f"<ul>", file=html)
+                        ul = True
+                    print(f"<li>{line[idx+1:-1]}</li>", file=html)
+                    if fidx + 1 == l and ul is True:
+                        print(f"</ul>", file=html)
 
         exit(0)
     except FileNotFoundError:
