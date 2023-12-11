e44c3683f4a45763101dc5d9596d9bf83f6356d4
PhYdrogen <Fossecruor@gmail.com>
2023-12-11 15:22:09 +0000

    feat: add paragraphs

--- markdown2html.py
+++ markdown2html.py
@@ -9,28 +9,31 @@ def main():
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
     try:
+        tags = ["*", "-", "#", "\n"]
         ul = False
         ol = False
+        p = False
         file = open(sys.argv[1], "r")
         file_len = open(sys.argv[1], "r")
         html = open(sys.argv[2], "w")
         l = len(file_len.readlines())
         for (fidx, line) in enumerate(file):
             for (idx, char) in enumerate(line):
-                if char == " " and line[idx-1] == "#":
-                    if ul is True:
+                if char.isspace() and idx == 0:
+                    if ul:
                         print(f"</ul>", file=html)
                         ul = False
-                    if ol is True:
+                    if ol:
                         print(f"</ol>", file=html)
                         ol = False
+                if char == " " and line[idx-1] == "#":
                     print(f"<h{idx}>{line[idx:-1]}</h{idx}>", file=html)
                 if char == " " and line[idx-1] == "-":
                     if ul is False:
                         print(f"<ul>", file=html)
                         ul = True
                     print(f"<li>{line[idx+1:-1]}</li>", file=html)
-                    if fidx + 1 == l and ul is True:
+                    if fidx + 1 == l and ul:
                         print(f"</ul>", file=html)
                         ul = False
                 if char == " " and line[idx-1] == "*":
@@ -38,9 +41,23 @@ def main():
                         print(f"<ol>", file=html)
                         ol = True
                     print(f"<li>{line[idx+1:-1]}</li>", file=html)
-                    if fidx + 1 == l and ol is True:
+                    if fidx + 1 == l and ol:
                         print(f"</ol>", file=html)
                         ol = False
+                # print(f"c: {char}")
+                if char.isspace() and p and idx == 0:
+                    print("</p>", file=html)
+                    p = False
+                if char not in tags and idx == 0:
+                    if p:
+                        print("<br/>", file=html)
+                    if p is False:
+                        print(f"<p>", file=html)
+                        p = True
+                    print(f"{line[:-1]}", file=html)
+                    if fidx + 1 == l and p:
+                        print("</p>", file=html)
+
         exit(0)
     except FileNotFoundError:
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
