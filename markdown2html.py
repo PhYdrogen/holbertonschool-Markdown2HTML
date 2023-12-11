14d0626126f6cf3b71365c2d59ab4db61e20b544
PhYdrogen <Fossecruor@gmail.com>
2023-12-11 13:45:20 +0000

    feat: parser of header

--- markdown2html.py
+++ markdown2html.py
@@ -10,7 +10,12 @@ def main():
         exit(1)
     try:
         file = open(sys.argv[1], "r")
-        # print(file)
+        html = open(sys.argv[2], "w")
+        for line in file:
+            for (idx, char) in enumerate(line):
+                if char == " " and line[idx-1] == "#":
+                    print(f"<h{idx}>{line[idx:-1]}</h{idx}>", file=html)
+
         exit(0)
     except FileNotFoundError:
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
