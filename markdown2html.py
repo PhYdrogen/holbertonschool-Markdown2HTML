f5015c9a7e67d26ea149a1cef4b72c390e3ad836
PhYdrogen <Fossecruor@gmail.com>
2023-12-11 12:57:03 +0000

    fix: correct order and add exit

--- markdown2html.py
+++ markdown2html.py
@@ -8,12 +8,13 @@ def main():
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
-
     try:
-        file = open("README.md")
+        file = open(sys.argv[1], "r")
+        # print(file)
         exit(0)
-    except:
+    except FileNotFoundError:
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
+        exit(1)
 
 
 if __name__ == "__main__":
